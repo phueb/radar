e8d7a6fbdebd26d103f0563bd852fb752a59a56c
phueb001@ucr.edu <Minastirith1111>
2018-07-22 11:07:02 -0600

    initial commit

---
+++ src/app.py
@@ -0,0 +1,169 @@
+import numpy as np
+import io
+import serial
+import math
+from flask import Flask, render_template, jsonify, request
+from bokeh.plotting import figure
+from bokeh.embed import components
+from bokeh.models.sources import AjaxDataSource, ColumnDataSource
+from bokeh.resources import INLINE
+
+MOCK_DATA = True
+SERIAL_URL = 'COM3'
+
+# data
+LINE_LENGTH = 6
+BAUD_RATE = 9600
+DISTANCE_UNITS = 'cm'
+NUM_STEPS = 32
+SENSOR_RANGE = [0, 200]
+MS_PER_STEP = 250  # smaller-> more frequent updates but risks breaking stream into too small chunks
+
+# plot
+ROLLOVER = 6
+SIZE = 600
+PAD = 0.1
+SCATTER_RADIUS = 0.05
+LINE_WIDTH = 2
+NUM_LINES = 5
+
+app = Flask(__name__)
+app.config.from_pyfile('server_configs.py')
+
+if MOCK_DATA:
+    stream = io.open('mock.txt', 'rb')
+    url_suffix = 'mock_data'
+else:
+    ser = serial.serial_for_url(SERIAL_URL, timeout=0, baudrate=BAUD_RATE)
+    ser.flushInput()
+    stream = io.BufferedRWPair(ser, ser)
+    url_suffix = 'data'
+sio = io.TextIOWrapper(stream, line_buffering=True)
+
+
+def convert(st, dist):
+    deg = st / NUM_STEPS * 360
+    rad = math.radians(deg)
+    interp_dist = np.interp(dist, SENSOR_RANGE, [0, 1])
+    result = - np.sin(rad) * interp_dist, np.cos(rad) * interp_dist
+    return result
+
+
+def make_plot():
+    def make_unit_poly_vertices(dist):
+        x0, y0 = 0, 0
+        theta = np.linspace(0, 2 * np.pi, NUM_STEPS + 1, endpoint=True)
+        theta += np.pi / 2  # start zero at top
+        result = [(dist * np.cos(t) + x0, dist * np.sin(t) + y0) for t in theta]
+        return result
+
+    p = figure(plot_width=SIZE,
+               plot_height=SIZE,
+               x_range=(-1 - PAD, 1 + PAD),
+               y_range=(-1 - PAD, 1 + PAD),
+               min_border=0,
+               background_fill_color="#000000")
+    p.xgrid.visible = False
+    p.ygrid.visible = False
+    p.xaxis.axis_label = 'Distance ({})'.format(DISTANCE_UNITS)
+    p.xaxis.axis_label = 'Distance ({})'.format(DISTANCE_UNITS)
+    p.xaxis.ticker = [-1, 0, 1]
+    p.yaxis.ticker = [-1, 0, 1]
+    p.xaxis.major_label_overrides = {-1: str(-SENSOR_RANGE[-1]), 0: 'Zero', 1: str(SENSOR_RANGE[-1])}
+    p.yaxis.major_label_overrides = {-1: str(-SENSOR_RANGE[-1]), 0: 'Zero', 1: str(SENSOR_RANGE[-1])}
+    p.toolbar.logo = None
+    p.toolbar_location = None
+
+    # concentric lines
+    distances_to_line = np.linspace(0, 1, NUM_LINES, endpoint=True)
+    for d in distances_to_line:
+        vertices = make_unit_poly_vertices(d)
+        line_x = [v[0] for v in vertices]
+        line_y = [v[1] for v in vertices]
+        line_source = ColumnDataSource({'x': line_x,
+                                        'y': line_y})
+        p.line(x='x',
+               y='y',
+               source=line_source,
+               line_width=LINE_WIDTH,
+               line_color='#43ff00')
+
+    # radial axes
+    num_axes = 4
+    big_angle = 2.0 * np.pi / num_axes
+    angles = np.arange(num_axes) * big_angle - np.pi / 2
+    p.annular_wedge(x=0,
+                    y=0,
+                    inner_radius=distances_to_line[0],
+                    outer_radius=distances_to_line[-1],
+                    start_angle=angles,
+                    end_angle=angles,
+                    color='#43ff00')
+
+    # scatter
+    # scatter_source = AjaxDataSource(data_url=request.url_root + url_suffix,  # TODO test on hostgator
+    scatter_source = AjaxDataSource(data_url="http://localhost:5000/mock_data",
+                                    polling_interval=MS_PER_STEP,
+                                    mode='replace')  # TODO test replace
+    scatter_source.data = {'x': [],
+                           'y': []}
+    p.scatter(x='x',
+              y='y',
+              source=scatter_source,
+              line_color=None,
+              fill_color='#43ff00',
+              radius=SCATTER_RADIUS)
+
+    # return components(p)
+    return p  # TODO
+
+
+@app.route('/')
+def index():
+    p = make_plot()
+    # return render_template("index.html", plot=p)
+
+    # TODO
+
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+
+    script, div = components(p, INLINE)
+
+    return render_template('index.html',
+                           plot_script=script,
+                           plot_div=div,
+                           js_resources=js_resources,
+                           css_resources=css_resources
+                           )
+
+
+@app.route('/data', methods=['GET', 'POST'])
+def data():
+    buffer_size = ser.inWaiting()
+    buffer = sio.read(buffer_size)
+    x = []
+    y = []
+    for line in buffer.split('\n'):
+        if len(line) == LINE_LENGTH:
+            print(line)
+            step, distance = line.strip('\n').split()
+            xy = convert(float(step), float(distance))
+            x.append(xy[0])
+            y.append(xy[1])
+    return jsonify(x=x[-1], y=y[-1])  # TODO must be one x, y pair
+
+
+@app.route('/mock_data', methods=['POST'])
+def mock_data():
+    line = sio.readline()
+    if not line:
+        sio.seek(0)
+        line = sio.readline()
+    print('line:', line, '\n')
+    step, distance = line.strip('\n').split()
+    x, y = convert(float(step), float(distance))
+    return jsonify({'x': [x], 'y': [y]})
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True, host='0.0.0.0')
